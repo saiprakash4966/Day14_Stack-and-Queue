9ba0eb480849991f7fba3884bf5a6bf21a085b3d
unknown <saiprakashbeemari@gmail.com>
2022-03-06 22:25:00 +0530

    Stack and Queue

--- Stack.java
+++ Stack.java
@@ -2,27 +2,24 @@ package com.bl;
 
 public class Stack 
 {
+	
 		MyNode head;
-
 
 		/*
 		 * 1. created addNode Method to add elements
 		 */
-		public void addNode(int data) 
-		{
+		public void addNode(int data) {
 			/*
 			 * Created node Object of MyNode class
 			 */
 			MyNode node = new MyNode(data);
-			
+
 			/*
 			 * If Stack is empty, head will point to the node
 			 */
-			if (head == null) 
-			{
+			if (head == null) {
 				head = node;
-			} else 
-			{
+			} else {
 				/*
 				 * node will be added after head such that head's next will point to Node
 				 */
@@ -43,6 +40,31 @@ public class Stack
 			}
 			System.out.println("");
 		}
+
+		/*
+		 * Created Method Peak it will returns the value of what is on the top of the
+		 * stack
+		 */
+		public void peak() {
+			MyNode peakValue = head;
+			System.out.println("Top element is :");
+			System.out.println(head.data);
+		}
+
+		/*
+		 * pop: Created Method deleteHead will remove the value of the stack and then
+		 * return it
+		 */
+		public void deleteHead()
+		{
+			System.out.println("deleting top node");
+			MyNode temp = head;
+			if (temp != null) {
+				head = temp.next;
+			}
+
+		
+	}
 	}
 
 
--- StackNew.java
+++ StackNew.java
@@ -2,35 +2,53 @@ package com.bl;
 
 public class StackNew 
 {
-		public static void main(String[] args)
-		{
 
-			/*
-			 * PROCEDURE :
-			 * 
-			 * 1.creating an Object of class Stack
-			 * 2.Calling method to push value to stack
-			 * 3.calling method to display value of stack
-			 */
+	public static void main(String[] args) 
+	{
 
-			/*
-			 * 1.creating Object an Object
-			 */
-			System.out.println("inserting element into stack ::");
-			Stack stack = new Stack();
-			
-			 /*
-			 * 2.Calling add node method to push value to stack
-			 */
-			
-			/*
-			 * UC1:Ability to create a Stack of 56->30->70
-			 */
-				stack.addNode(70);
-				stack.addNode(30);
-				stack.addNode(56);
+		/*
+		 * PROCEDURE :
+		 * 
+		 * 1.creating an Object of class Stack
+		 * 2.Calling method to push value to stack
+		 * 3.calling method to display value of stack
+		 */
 
-		}
+		/*
+		 * deleting stack head element
+		 */
+		System.out.println("deleting element ::");
+		/*
+		 * 1.creating an Oject of class Stack
+		 */
+		Stack stack = new Stack();
+
+		/*
+		 * Calling mathod addNode to add elements
+		 */
+		stack.addNode(70);
+		stack.addNode(30);
+		stack.addNode(56);
+
+		/*
+		 * Calling Method Peak it will returns the value of what is on the top of the
+		 * stack
+		 */
+		stack.peak();
+
+		/*
+		 * pop: Created Method deleteHead will remove the value of the stack and then
+		 * return it
+		 */
+		stack.deleteHead();
+		stack.printList();
+		stack.peak();
+		stack.deleteHead();
+		stack.printList();
+		stack.peak();
+		stack.deleteHead();
+
 	}
+}
 
 
